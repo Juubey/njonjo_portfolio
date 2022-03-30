dbe61da218266d4d36740a6e0058265a1ce0363e
Juubey <albert.njubi1@gmail.com>
2022-03-30 15:51:03 +0200

    CamelCaseFix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ const { TextArea } = Input;
 
 function Contact(){
 return (
-  <contactBlock>
+  <ContactBlock>
     <div className="containter-fluid">
       <div className="titleHolder">
         <SectionTitle main>Let's connect!</SectionTitle>
@@ -81,7 +81,7 @@ return (
     <div/>
 
     </div>
-  </contactBlock>
+  </ContactBlock>
   );
 }
 
