a43a9b93238fd06f0b3130f95c53896f1a3ef7ad
Juubey <albert.njubi1@gmail.com>
2022-04-05 18:50:33 +0200

    Added WebGL Builds to the visit section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,14 +9,18 @@ const Hero = () => (
     <LeftSection>
       <SectionTitle main center>
         Hi there, I'm Njonjo,<br />
-        a UnityEngine Game Programmer & Designer. 
+        a Software and Game Developer. 
       </SectionTitle>
       <SectionText>
-      Please take a look around, see what I've been up to, and let's see what we can build together.
+      Please take a look around, see what I've been up to, and let's see what we can build together.<br />
+      <p>Browse some of my work.</p>
       </SectionText>
-      <Button onClick={() => window.location = 'https://github.com/Juubey' }>Need a Developer? </Button>
-      {/* <Button onClick={() => window.location = 'https://github.com/Juubey' }>Need a Designer? </Button> */}
-      
+      <ul>
+        <li>
+          <Button onClick={() => window.location = 'https://github.com/Juubey' }>Need a Software Developer?</Button>
+          <Button onClick={() => window.location = 'https://albert-njubi.itch.io/' }>Need a Game Dev?</Button>
+        </li>
+      </ul>
     </LeftSection>
 
   </Section>
